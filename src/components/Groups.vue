20502455114cb5fced8c72281a0a8de95961ab95
leonawolff <leona.wolff.ok@gmail.com>
2021-04-24 22:38:39 +0100

    Dialog box not working

--- src/components/Groups.vue
+++ src/components/Groups.vue
@@ -8,9 +8,57 @@
                 <div class="card-body">
                     <div>Bees</div>
                     <br/>
-                    <div class="text-center">
-                        <v-btn elevation="2" v-bind="attrs" v-on="on">New Group</v-btn>
-                    </div>
+                    <template>
+                        <v-row>
+                            <v-dialog
+                            v-model="dialog"
+                            persistent
+                            max-width="600px"
+                            >
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn
+                                    color="primary"
+                                    dark
+                                    v-bind="attrs"
+                                    v-on="on"
+                                    @click="dialog = true"
+                                    >
+                                    New Group
+                                    </v-btn>
+                                </template>
+                                <v-card>
+                                    <v-card-title>
+                                        <span class="headline">New Group</span>
+                                    </v-card-title>
+                                    <v-card-text>
+                                        <v-container>
+                                                <v-text-field
+                                                label="Group Name"
+                                                required
+                                                ></v-text-field>
+                                        </v-container>
+                                    </v-card-text>
+                                    <v-card-actions>
+                                        <v-spacer></v-spacer>
+                                        <v-btn
+                                            color="blue darken-1"
+                                            text
+                                            @click="dialog = false"
+                                        >
+                                            Close
+                                        </v-btn>
+                                        <v-btn
+                                            color="blue darken-1"
+                                            text
+                                            @click="dialog = false"
+                                        >
+                                            Save
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-card>
+                            </v-dialog>
+                        </v-row>
+                    </template>
                 </div>
             </template>
             <template v-else>
@@ -24,20 +72,15 @@
 <script>
 import { mapGetters } from "vuex";
 export default {
-  computed: {
-    // map `this.user` to `this.$store.getters.user`
-    ...mapGetters({
-      user: "user"
-    })
-  }
-};
+    computed: {
+        // map `this.user` to `this.$store.getters.user`
+        ...mapGetters({
+        user: "user"
+        }),
+    }
+//,
+//     data: () => ({
+//         dialog: false,
+//     }),
+   };
 </script>
-
-
-
-// function makeGroup(title, userEmail){
-//     return {
-//         title: title,
-//         userEmail: userEmail
-//     }
-// }
